9906378b8cad8a35614dfceeec86c3f862eb3958
Chirag Nagrekar <chiragce17@gmail.com>
2020-10-09 19:15:59 +0530

    Update Check-RemoteConnectivity.ps1

--- Check-RemoteConnectivity.ps1
+++ Check-RemoteConnectivity.ps1
@@ -1,57 +1,73 @@
-Write-Output "`nRunning Connection Check "
-    $output = @()
-    $pingcon = ""
-    $winrmcon = ""
-    $Policy = ""
-    $HttpPort = ""
-    $HttpSPort = ""
-    $PublicProfileStatus = ""
-    $DomainProfileStatus = ""
-    $PrivateProfileStatus = ""
+function check-RemoteConnectivity {
+    param(
+        [parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [String[]]$servers
+    )
 
-    foreach($server in (Get-Content .\Servers.txt)){
 
-        Write-Output "`nRunning connection check for $server"
+    Begin {
+        Write-Verbose "`nRunning Connection Check "
+        $output = @()
+        $pingcon = ""
+        $winrmcon = ""
+        $Policy = ""
+        $HttpPort = ""
+        $HttpSPort = ""
+        $PublicProfileStatus = ""
+        $DomainProfileStatus = ""
+        $PrivateProfileStatus = ""
+        $outputpath = "ConnectionOutput.csv"
+    }
+
+    Process {
+        foreach ($server in $servers) {
+
+            Write-Output "`nRunning connection check for $server"
          
-        if(Test-Connection -ComputerName $server -Count 2 -Quiet -ErrorAction Ignore){$pingcon = "Reachable"}
-        else{$pingcon = "NotReachable"}
+            if (Test-Connection -ComputerName $server -Count 2 -Quiet -ErrorAction Ignore) { $pingcon = "Reachable" }
+            else { $pingcon = "NotReachable" }
 
-        if(Test-WSMan -ComputerName $server -ErrorAction Ignore){$winrmcon = "working"}
-        else{$winrmcon = "NotWorking"}
+            if (Test-WSMan -ComputerName $server -ErrorAction Ignore) { $winrmcon = "working" }
+            else { $winrmcon = "NotWorking" }
 
-        $Policy = (Invoke-Command -ComputerName $server -Scriptblock{Get-ExecutionPolicy} -ErrorAction Ignore).Value 
+            $Policy = (Invoke-Command -ComputerName $server -Scriptblock { Get-ExecutionPolicy } -ErrorAction Ignore).Value 
 
-        $HttpPort = (Test-NetConnection $server -Port 5985).Tcptestsucceeded
-        $HttpsPort = (Test-NetConnection $server -Port 5986).Tcptestsucceeded
+            $HttpPort = (Test-NetConnection $server -Port 5985).Tcptestsucceeded
+            $HttpsPort = (Test-NetConnection $server -Port 5986).Tcptestsucceeded
 
-        if($winrmcon -eq "Working"){
-                $FirewallProf = Invoke-Command -ComputerName $server -ScriptBlock{Get-NetFirewallProfile | Select Name, Enabled}
-                $PublicProfileStatus = ($FirewallProf | where{$_.Name -eq "Public"}).Enabled.Value
-                $PrivateProfileStatus = ($FirewallProf | where{$_.Name -eq "Private"}).Enabled.Value 
-                $DomainProfileStatus = ($FirewallProf | where{$_.Name -eq "Domain"}).Enabled.Value
+            if ($winrmcon -eq "Working") {
+                $FirewallProf = Invoke-Command -ComputerName $server -ScriptBlock { Get-NetFirewallProfile | Select Name, Enabled }
+                $PublicProfileStatus = ($FirewallProf | where { $_.Name -eq "Public" }).Enabled.Value
+                $PrivateProfileStatus = ($FirewallProf | where { $_.Name -eq "Private" }).Enabled.Value 
+                $DomainProfileStatus = ($FirewallProf | where { $_.Name -eq "Domain" }).Enabled.Value
             }
         
 
-        else{
-            $PublicProfileStatus = "Unknown"
-            $DomainProfileStatus = "Unknown"
-            $PrivateProfileStatus = "Unknown"
-        }
+            else {
+                $PublicProfileStatus = "Unknown"
+                $DomainProfileStatus = "Unknown"
+                $PrivateProfileStatus = "Unknown"
+            }
         
-        $output += [PSCustomObject]@{
-            "Server_Name" = $server
-            "Ping_Connection" = $pingcon
-            "WinRM_Conenction" = $winrmcon
-            "Policy" = $Policy
-            "WINRM_HTTP_Port_Open" = $HttpPort
-            "WINRM_HTTPS_Port_Open" = $HttpSPort
-            "Firewall_Public_Prof_Enabled?" = $PublicProfileStatus
-            "Firewall_Private_Prof_Enabled?" = $PrivateProfileStatus
-            "Firewall_Domain_Prof_Enabled?" = $DomainProfileStatus
-        }
+            $output += [PSCustomObject]@{
+                "Server_Name"                    = $server
+                "Ping_Connection"                = $pingcon
+                "WinRM_Conenction"               = $winrmcon
+                "Policy"                         = $Policy
+                "WINRM_HTTP_Port_Open"           = $HttpPort
+                "WINRM_HTTPS_Port_Open"          = $HttpSPort
+                "Firewall_Public_Prof_Enabled?"  = $PublicProfileStatus
+                "Firewall_Private_Prof_Enabled?" = $PrivateProfileStatus
+                "Firewall_Domain_Prof_Enabled?"  = $DomainProfileStatus
+            }
         
+        }
+
     }
 
-    $output | ft -AutoSize
-    $output | Export-Csv .\ConnectionOutput.csv -NoTypeInformation
-    Write-Output "`nOutput is stored to ConnectionOutput.Csv in the same script folder"
+    End {
+        $output | ft -AutoSize
+        $output | Export-Csv .\ConnectionOutput.csv -NoTypeInformation
+        Write-Output "`nOutput is stored at $(Join-Path -Path (Get-Location).Path -ChildPath $outputpath) location"
+    }
+}
